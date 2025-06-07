3219e0b9595c37a87666262734cbbcbd42deda6c
willowcoleman <59978407+willowcoleman@users.noreply.github.com>
2025-06-07 22:09:59 +0000

    Deploying to gh-pages from @ willowcoleman/willowcoleman.github.io@0905b437dd8f0ffd1b8523ab1883f32e6a26eef0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,14 +501,21 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%72%77%63%6F%6C%65%6D%61%6E@%67.%75%63%6C%61.%65%64%75", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://www.linkedin.com/in/red-willow-coleman", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/0000-0001-6582-4922", "_blank");
         },
       },{
         id: 'social-rss',
@@ -522,14 +529,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=FNhFOjUAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
